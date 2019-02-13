87edadedaef0e3a147ba0ed6cd6e81402c66760a
Sullyvan Puppi <spuppi@gmail.com>
2019-02-13 17:36:53 -0200

    Inserido arquivo readme.md com descrição do projeto.

---
+++ contatos-app/src/main/java/com/spuppi/contatosapp/repository/Contatos.java
@@ -0,0 +1,11 @@
+package com.spuppi.contatosapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spuppi.contatosapp.model.Contato;
+
+public interface Contatos extends JpaRepository<Contato, Long>{
+
+	
+		
+}
