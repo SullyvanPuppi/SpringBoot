636513fafb424b8db21290873297c59e59271064
Sullyvan Puppi <spuppi@gmail.com>
2019-02-15 18:48:41 -0200

    Atualização da descrição do Projeto API REST Filmes.

--- apirest-filmes/src/main/java/com/spuppi/apirestfilmes/ApirestFilmesApplication.java
+++ apirest-filmes/src/main/java/com/spuppi/apirestfilmes/ApirestFilmesApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class ApirestFilmesApplication {
+//public class ApirestFilmesApplication extends SpringBootServletInitializer{ 
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApirestFilmesApplication.class, args);
