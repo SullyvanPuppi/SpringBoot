ee1d9da34d8cffc3306a210fa178c7040019eb0b
Sullyvan Puppi <spuppi@gmail.com>
2019-02-20 10:03:22 -0300

    Import projeto base Spring para Cliente API RESTfilmes.

---
+++ cliente-apirestfilmes/src/main/java/com/spuppi/clienteapirestfilmes/ClienteApirestfilmesApplication.java
@@ -0,0 +1,13 @@
+package com.spuppi.clienteapirestfilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteApirestfilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteApirestfilmesApplication.class, args);
+	}
+
+}
---
+++ cliente-apirestfilmes/src/test/java/com/spuppi/clienteapirestfilmes/ClienteApirestfilmesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spuppi.clienteapirestfilmes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClienteApirestfilmesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
